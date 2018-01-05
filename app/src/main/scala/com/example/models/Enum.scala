5c53b7b37aa43f6e53fcd1ba8f9131a8ab2f2480
Tomofumi Tanaka <tanacasino@gmail.com>
2018-01-05 17:28:10 +0900

    override

--- app/src/main/scala/com/example/models/Enum.scala
+++ app/src/main/scala/com/example/models/Enum.scala
@@ -10,9 +10,9 @@ abstract class EnumCompanion[A, B <: Enum[A]] {
   def values: Seq[B]
   def valueOf(code: A): Option[B] = values.find(_.code == code)
 
-  final implicit def enumPrism: Prism[A, B] = new Prism[A, B] {
+  implicit def enumPrism: Prism[A, B] = new Prism[A, B] {
     override def toOpt(a: A): Option[B] = valueOf(a)
-    def from(b: B): A = b.code
+    override def from(b: B): A = b.code
   }
 
 }
