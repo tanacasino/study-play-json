1b80c73151e2ca52d14c951cd1f8a227f068ad87
Tomofumi Tanaka <tanacasino@gmail.com>
2018-01-05 17:26:02 +0900

    Add note

--- libs/src/main/scala/com/example/libs/json/ExtractKeys.scala
+++ libs/src/main/scala/com/example/libs/json/ExtractKeys.scala
@@ -3,16 +3,21 @@ package com.example.libs.json
 import scala.reflect.macros.whitebox
 import scala.language.experimental.macros
 
+/**
+  *
+  * こちらを参考にplay-jsonのマクロで作成した Writes に Optionのフィールドに明示的にnullを突っ込む対応を
+  * するためのマクロを使った実装を真似たもの
+  *
+  * https://groups.google.com/forum/#!msg/play-framework/b5xF1GdwQGI/PjO7bVDxoHwJ
+  */
 object ExtractKeys {
 
   def optionalClassParamsAsList[T: c.WeakTypeTag](c: whitebox.Context): c.Expr[List[String]] = {
     import c.universe._
 
-
     val companioned = weakTypeOf[T].typeSymbol
     val companionSymbol = companioned.companion
     val companionType = companionSymbol.typeSignature
-
 
     companionType.decl(TermName("apply")) match {
       case NoSymbol => c.abort(c.enclosingPosition, "No apply function found")
@@ -21,21 +26,24 @@ object ExtractKeys {
         val params = apply.paramLists.head
         val paramsNullable = params filter (_.typeSignature.typeConstructor <:< typeOf[Option[_]].typeConstructor) map (_.name.toString)
 
-
         val listApply = if (paramsNullable.nonEmpty)
           s""" "${paramsNullable.mkString("""","""")}" """
         else
           ""
 
-
         val tree = c.parse(s"List($listApply)")
-
 
         c.typecheck(tree)
         c.Expr[List[String]](tree)
     }
   }
 
+  /**
+    * 指定された [[T]] クラスのOptionのフィールドのキー名一覧を抽出する
+    *
+    * @tparam T
+    * @return キー名のリスト
+    */
   def extractKeysOpt[T]: List[String] = macro ExtractKeys.optionalClassParamsAsList[T]
 
 }
--- libs/src/main/scala/com/example/libs/json/syntax/package.scala
+++ libs/src/main/scala/com/example/libs/json/syntax/package.scala
@@ -4,6 +4,12 @@ import play.api.libs.json.{JsNull, OWrites}
 
 package object syntax {
 
+  /**
+    * OWritesとOptionの項目キー名を受け取って、JSONにシリアライズする
+    * Optionのキー名がJsObjectに存在しない場合に明示的に JsNullで突っ込む
+    * @param ow
+    * @tparam A
+    */
   final implicit class OWritesOps[A](val ow: OWrites[A]) {
     def omitNull(keysNullable: List[String]): OWrites[A] = OWrites { o =>
       keysNullable.foldLeft(ow.writes(o)) {
